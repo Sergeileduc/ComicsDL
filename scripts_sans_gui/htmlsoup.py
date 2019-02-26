397d3db856409ead10d2cbd330901794364f1f5c
Sergeileduc <sergei.leduc@gmail.com>
2019-02-27 00:33:28 +0100

    remove code in comment

--- scripts_sans_gui/htmlsoup.py
+++ scripts_sans_gui/htmlsoup.py
@@ -6,27 +6,9 @@ import requests
 import urllib.request
 import urllib.error
 
-# # get html from url
-# def returnHTML(url):
-#     user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
-#     headers = { 'User-Agent' : user_agent }
-#     #hdr = {'Accept': 'text/html', 'User-Agent': "Fiddler"}
-#     try:
-#         req = urllib.request.Request(url, None, headers)
-#         response = urllib.request.urlopen(req)
-#         html = response.read()
-#         response.close()
-#         return html
-#     except ValueError as e:
-#         print("returnHTML error")
-#         print(e)
-#         raise
-#     except urllib.error.HTTPError as e:
-#         print("returnHTML error")
-#         print(e)
-#         raise
-
 #def url 2 soup
+
+
 def url2soup(url):
     try:
         res = requests.get(url)
@@ -46,9 +28,12 @@ def url2soup(url):
         raise
 
 #get beautiful soup
+
+
 def html2soup(html):
     soup = BeautifulSoup(html, 'html.parser')
     return soup
+
 
 def getaALLhref(html, tag):
     urllist = list()
