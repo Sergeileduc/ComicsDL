f57fcf7afdb4fc2da649a6d1d4951ada6d0da57e
Sergeileduc <sergei.leduc@gmail.com>
2019-02-28 10:09:09 +0100

    Lint code

--- scripts_sans_gui/getcomics.py
+++ scripts_sans_gui/getcomics.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # -*-coding:utf-8 -*-
 
-import requests #html
+import requests  # html
 import urllib.request
 import urllib.error
 from datetime import datetime
@@ -15,22 +15,26 @@ today = datetime.today().strftime("%Y-%m-%d")
 
 BASE = "https://getcomics.info/go.php-url=/"
 
-#find las weekly post
+user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+
+
+# Find las weekly post
 def findLastWeekly(url):
     soup = htmlsoup.url2soup(url)
     lastPost = soup.find('article', class_='type-post')
-    #check if today's archive is there, and retrieve its url
+    # Check if today's archive is there, and retrieve its url
     print ("Latest weekly post: " + lastPost.time['datetime'])
     if today in lastPost.time['datetime']:
-        #print ('There is a new one today. Hurrah!')
+        # Print ('There is a new one today. Hurrah!')
         pass
     else:
-        #print ('Nothing yet. Exiting...')
-        #print ('Continue anyway...')
-        #quit()
+        # print ('Nothing yet. Exiting...')
+        # print ('Continue anyway...')
+        # quit()
         pass
     postUrl = lastPost.h1.a['href']
     return postUrl
+
 
 def comicsList(url):
     weeklyUrl = findLastWeekly(url)
@@ -40,10 +44,11 @@ def comicsList(url):
     interm2 = soup2.find_all('li')
     return htmlsoup.getaALLhref(str(interm2), 'a')
 
-#find download link
+
+# Find download link
 def downCom(url):
-    user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
-    headers = { 'User-Agent' : user_agent }
+    global user_agent
+    headers = {'User-Agent': user_agent}
     try:
         req = urllib.request.Request(url, None, headers)
         finalurl = urllib.request.urlopen(req).geturl()
@@ -58,17 +63,18 @@ def downCom(url):
     except Exception as e:
         print(e)
     for button in downButtons:
-        #if 'zippyshare' in str(button).lower() and 'href' in button.a.attrs:
-        if 'zippyshare' in button.get("href") or 'zippyshare' in button.get('title').lower():
+        # if 'zippyshare' in str(button).lower() and 'href' in button.a.attrs:
+        if 'zippyshare' in button.get("href") \
+                or 'zippyshare' in button.get('title').lower():
             zippylink = button.get("href")
             print(zippylink)
             try:
                 if str(zippylink).startswith(BASE):
                     print("Abracadabra !")
-                    finalzippy = base64.b64decode(zippylink[len(BASE):]).decode()
+                    finalzippy = base64.b64decode(
+                            zippylink[len(BASE):]).decode()
                 else:
-                    user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
-                    headers = { 'User-Agent' : user_agent }
+                    headers = {'User-Agent': user_agent}
                     req = urllib.request.Request(zippylink, None, headers)
                     finalzippy = urllib.request.urlopen(req).geturl()
             except urllib.error.HTTPError as e:
@@ -83,24 +89,27 @@ def downCom(url):
             except Exception as e:
                 print("error in downComZippy")
                 print(e)
-    #except urllib.error.HTTPError:
-        #print("downCom got HTTPError from returnHTML")
-        #raise
+    # except urllib.error.HTTPError:
+        # print("downCom got HTTPError from returnHTML")
+        # raise
     return
 
-#download from zippyshare
+
+# Download from zippyshare
 def downComZippy(url):
     soup = htmlsoup.url2soup(url)
-    #downButton = soup.select("script[type='text/javascript']")
-    #downButton = soup.select("table[class='folderlogo'] > tr > td")[0].find("div", style=re.compile("margin-left"))
+    # downButton = soup.select("script[type='text/javascript']")
+    # downButton = soup.select(
+    #         "table[class='folderlogo'] > tr > td")[0].find(
+    #                 "div", style=re.compile("margin-left"))
     downButton = soup.find('a', id="dlbutton").find_next_sibling().text
-    #.find("script", type="text/javascript")
-    #.find("div", style=re.compile("width: 303px;"))
-    #downButton = soup.find("script", type="text/javascript")
+    # .find("script", type="text/javascript")
+    # .find("div", style=re.compile("width: 303px;"))
+    # downButton = soup.find("script", type="text/javascript")
     # print(downButton)
-    #interm = soup.select("div.right")
-    #soup2 = BeautifulSoup(str(interm), 'html.parser')
-    #downButton = soup2.select('script[type="text/javascript"]')
+    # interm = soup.select("div.right")
+    # soup2 = BeautifulSoup(str(interm), 'html.parser')
+    # downButton = soup2.select('script[type="text/javascript"]')
     try:
         fullURL, fileName = zpshare.getZippyDL(url, downButton)
         print ("Downloading from zippyshare into : " + fileName)
@@ -111,7 +120,7 @@ def downComZippy(url):
         print(e)
         print("Can't get download link on zippyshare page")
 
-    #download from url & trim it a little bit
+    # Download from url & trim it a little bit
     with open(fileName, 'wb') as f:
         try:
             for block in r.iter_content(1024):
