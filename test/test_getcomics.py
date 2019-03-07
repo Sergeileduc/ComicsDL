ecb29728037e01800eb8f33bd02d2fbf29c15640
Sergeileduc <sergei.leduc@gmail.com>
2019-03-07 10:22:48 +0100

    unit test PEP8

--- test/test_getcomics.py
+++ test/test_getcomics.py
@@ -2,7 +2,9 @@
 # -*-coding:utf-8 -*-
 
 import unittest
-from utils.getcomics import *
+from utils.getcomics import findLastWeekly
+from utils.getcomics import comicsList
+
 
 class TestFonctionGet(unittest.TestCase):
 
@@ -16,9 +18,9 @@ class TestFonctionGet(unittest.TestCase):
     # est un test.
     def test_findLastWeekly_DC(self):
 
-        result = 'https://getcomics.info/dc/2019-02-27-dc-week/'
+        # result = 'https://getcomics.info/dc/2019-02-27-dc-week/'
 
-        myurl = findLastWeekly(getcomicsurls[0])
+        myurl = findLastWeekly(self.getcomicsurls[0])
 
         print(myurl)
 
@@ -30,20 +32,19 @@ class TestFonctionGet(unittest.TestCase):
 
     def test_findLastWeekly_Marvel(self):
 
-        result = 'https://getcomics.info/marvel/2019-02-27-marvel-week/'
+        # result = 'https://getcomics.info/marvel/2019-02-27-marvel-week/'
 
-        myurl = findLastWeekly(getcomicsurls[1])
+        myurl = findLastWeekly(self.getcomicsurls[1])
 
         print(myurl)
 
         # self.assertEqual(myurl, result)
 
-
     def test_findLastWeekly_Indie(self):
 
-        result = 'https://getcomics.info/other-comics/2019-02-27-indie-week/'
+        # result = 'https://getcomics.info/other-comics/2019-02-27-indie-week/'
 
-        myurl = findLastWeekly(getcomicsurls[2])
+        myurl = findLastWeekly(self.getcomicsurls[2])
 
         print(myurl)
 
@@ -51,17 +52,19 @@ class TestFonctionGet(unittest.TestCase):
 
     def test_findLastWeekly_Image(self):
 
-        result = 'https://getcomics.info/other-comics/2019-02-27-image-week/'
+        # result = 'https://getcomics.info/other-comics/2019-02-27-image-week/'
 
-        myurl = findLastWeekly(getcomicsurls[3])
+        myurl = findLastWeekly(self.getcomicsurls[3])
 
         print(myurl)
 
         # self.assertEqual(myurl, result)
 
     def test_comicsList(self):
-        result = comicsList(getcomicsurls[0])
+        print("Comics list DC")
+        result = comicsList(self.getcomicsurls[0])
         print(result)
+
 
 # Ceci lance le test si on exécute le script
 # directement.
