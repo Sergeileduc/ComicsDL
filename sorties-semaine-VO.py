4a64db9b158d7035a4286867f224febfada82213
Sergeileduc <sergei.leduc@gmail.com>
2019-07-24 16:46:07 +0200

    Move constants

--- sorties-semaine-VO.py
+++ sorties-semaine-VO.py
@@ -6,19 +6,10 @@ import os
 
 from utils import htmlsoup, getcomics
 
-DC_URL = "https://getcomics.info/tag/dc-week/"
-MARVEL_URL = "https://getcomics.info/tag/marvel-now/"
-IMAGE_URL = "https://getcomics.info/tag/image-week/"
-INDIE_URL = "https://getcomics.info/tag/indie-week/"
-
-note = "Notes :\n"
-howto = "Video guide on how"
-howtodl = "how to download"
-consistof = "consist of :"
-lower = "or on the lower"
-indieweek = "INDIE Week+"
-bloat = ['Language :', 'Image Format :', 'Year :',
-         'Size :', 'Size : ', 'Notes :', 'Screenshots :']
+# Constants
+from utils.const import DC_URL, MARVEL_URL, IMAGE_URL, INDIE_URL
+from utils.const import note, howto, howtodl, consistof
+from utils.const import lower, indieweek, bloat
 
 
 # Find last weekly and display
