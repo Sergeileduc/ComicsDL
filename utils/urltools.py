8fb1ba2724ca577cf6e89e0e817ac14a1886b28b
Sergeileduc <sergei.leduc@gmail.com>
2019-07-27 11:15:46 +0200

    replace urllib in urltools

--- utils/urltools.py
+++ utils/urltools.py
@@ -1,18 +1,40 @@
 #!/usr/bin/python3
 # -*-coding:utf-8 -*-
 
-import urllib.request
+import requests
+# import urllib.request
 from urllib.error import HTTPError
 
 user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
 headers = {'User-Agent': user_agent}
 
 
-def getfinalurl(url):
+# Find the final url, if redirections occurs
+def getfinlaurl(url):
     try:
-        req = urllib.request.Request(url, None, headers)
-        finalurl = urllib.request.urlopen(req).geturl()
-        return finalurl
+        response = requests.get(url)
+        if response.history:
+            print("Request was redirected")
+            # for resp in response.history:
+            #     print(resp.status_code, resp.url)
+            # print("Final destination:")
+            # print(response.status_code, response.url)
+            # print(response.url)
+            return response.url
+        else:
+            # print("Request was not redirected")
+            return url
     except HTTPError:
-        print("downCom got HTTPError from Request")
-        raise
+            print("downCom got HTTPError")
+            return url
+            raise
+
+# Old method for geting final url
+# def getfinalurl(url):
+#     try:
+#         req = urllib.request.Request(url, None, headers)
+#         finalurl = urllib.request.urlopen(req).geturl()
+#         return finalurl
+#     except HTTPError:
+#         print("downCom got HTTPError from Request")
+#         raise
