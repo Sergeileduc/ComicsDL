fcc1d3f75ef4ba2db9d3420c1c73e38e5c2e2ec5
Sergeileduc <sergei.leduc@gmail.com>
2020-02-13 09:35:31 +0100

    Clean tests (zpshare)

--- tests/test_zpshare.py
+++ tests/test_zpshare.py
@@ -2,10 +2,33 @@
 # -*-coding:utf-8 -*-
 """Unit tests for utils/zpshare."""
 
-# import pytest
+import pandas as pd
+import pytest
+from utils.tools import search_regex_name
 from utils.zpshare import (get_file_url, _remove_tag,
-                           find_zippy_download_button)
+                           find_zippy_download_button,
+                           regex_abcd)
 # from utils import htmlsoup
+
+
+# Read abcd from file and make a list of tuples
+df = pd.read_csv('tests/test_zpshare/expected_abcd.csv', header=None)
+abcd = [tuple(row) for row in df.values]
+#  = [('a', 45372), ('b', 51245), ('c', 45372), ('d', 913)]
+
+
+@pytest.fixture
+def button_html(datadir):
+    contents = (datadir / 'button_script.txt').read_text()
+    return contents
+
+
+@pytest.mark.parametrize("field,expected", abcd)
+def test_search_regex_name(button_html, field, expected):
+    """Doc."""
+    print("youhu")
+    res = int(search_regex_name(button_html, regex_abcd, field))
+    assert res == expected
 
 
 def test_get_file_url():
