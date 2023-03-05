57d25e871ef96d556002728c6db4440a336561c4
Sergeileduc <sergei.leduc@gmail.com>
2023-03-05 11:50:19 +0100

    unit tests with pytest (deprecate unittest)

--- tests/test_getcomics.py
+++ tests/test_getcomics.py
@@ -2,79 +2,47 @@
 # -*-coding:utf-8 -*-
 """Unit tests for utils/getcomics."""
 
-import unittest
+import pytest
+
 from utils.getcomics import find_last_weekly, comics_list
 from utils.getcomics import searchurl, getresults
 
 
-class TestFonctionGet(unittest.TestCase):
-    """Tests for getcomics module."""
-
-    getcomicsurls = ['https://getcomics.info/tag/dc-week/',
-                     'http://getcomics.info/tag/marvel-now/',
-                     'https://getcomics.info/tag/indie-week/',
-                     'https://getcomics.info/tag/image-week/'
-                     ]
-
-    # Chaque méthode dont le nom commence par 'test_'
-    # est un test.
-    def test_find_last_weekly_DC(self):
-        """Test find_last_weekly."""
-        myurl = find_last_weekly(self.getcomicsurls[0])
-        print(myurl)
-
-        # Le test le plus simple est un test d'égalité. On se
-        # sert de la méthode assertEqual pour dire que l'on
-        # s'attend à ce que les deux éléments soient égaux. Sinon
-        # le test échoue.
-        # self.assertEqual(myurl, result)
-
-    def test_find_last_weekly_marvel(self):
-        """Test find_last_weekly."""
-        myurl = find_last_weekly(self.getcomicsurls[1])
-        print(myurl)
-        # self.assertEqual(myurl, result)
-
-    def test_find_last_weekly_indie(self):
-        """Test find_last_weekly."""
-        myurl = find_last_weekly(self.getcomicsurls[2])
-        print(myurl)
-        # self.assertEqual(myurl, result)
-
-    def test_find_last_weekly_image(self):
-        """Test find_last_weekly."""
-        myurl = find_last_weekly(self.getcomicsurls[3])
-        print(myurl)
-        # self.assertEqual(myurl, result)
-
-    def test_comicsList(self):
-        """Test comics_list()."""
-        print("Comics list DC")
-        result = comics_list(self.getcomicsurls[0])
-        print(result)
-
-    def test_searchurl_1(self):
-        """Test searchurl()."""
-        myurl = searchurl("Batman", 0, 2)
-        expected = "https://getcomics.info/tag/batman/page/2/"
-        print(myurl)
-        self.assertEqual(myurl, expected)
-
-    def test_searchurl_2(self):
-        """Test searchurl()."""
-        myurl = searchurl("New X-Men", 1, 3)
-        expected = "https://getcomics.info/page/3/?s=new+x-men"
-        print(myurl)
-        self.assertEqual(myurl, expected)
-
-    def test_get_results(self):
-        """Test getresults."""
-        searchurl = "https://getcomics.info/tag/batman/page/2/"
-        searchlist = getresults(searchurl)
-        print(searchlist)
+@pytest.fixture(scope="module",
+                params=['https://getcomics.info/tag/dc-week/',
+                        'http://getcomics.info/tag/marvel-now/',
+                        'https://getcomics.info/tag/indie-week/',
+                        'https://getcomics.info/tag/image-week/'
+                        ])
+def getcomics_url(request):
+    return request.param
 
 
-# Ceci lance le test si on exécute le script
-# directement.
-if __name__ == '__main__':
-    unittest.main()
+def test_find_last_weekly(getcomics_url):
+    """Test find_last_weekly."""
+    myurl = find_last_weekly(getcomics_url)
+    print(myurl)
+    assert myurl is not None
+
+
+def test_comicsList(getcomics_url):
+    """Test comics_list()."""
+    result = comics_list(getcomics_url)
+    print(result)
+    assert result is not None
+
+
+@pytest.mark.parametrize(
+    "input_,mode,page,expected",
+    [("Batman", 0, 2, "https://getcomics.info/tag/batman/page/2/"),
+     ("New X-Men", 1, 3, "https://getcomics.info/page/3/?s=new+x-men")])
+def test_searchurl(input_, mode, page, expected):
+    myurl = searchurl(input_, mode, page)
+    assert myurl == expected
+
+
+def test_get_results():
+    """Test getresults."""
+    searchurl = "https://getcomics.info/tag/batman/page/2/"
+    searchlist = getresults(searchurl)
+    print(searchlist)
---
+++ tests/test_zpshare.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+# -*-coding:utf-8 -*-
+"""Unit tests for utils/zpshare."""
+
+import pandas as pd
+import pytest
+from utils.tools import search_regex_name
+from utils.zpshare import (get_file_url, _remove_tag,
+                           find_zippy_download_button,
+                           regex_abcd)
+
+
+# Read abcd from file and make a list of tuples
+df = pd.read_csv('tests/test_zpshare/expected_abcd.csv', header=None)
+abcd = [tuple(row) for row in df.values]
+#  = [('a', 45372), ('b', 51245), ('c', 45372), ('d', 913)]
+
+
+@pytest.fixture
+def button_html(datadir):
+    contents = (datadir / 'button_script.txt').read_text()
+    return contents
+
+
+@pytest.mark.parametrize("field,expected", abcd)
+def test_search_regex_name(button_html, field, expected):
+    """Doc."""
+    res = int(search_regex_name(button_html, regex_abcd, field))
+    assert res == expected
+
+
+@pytest.mark.parametrize("url,expected_name",
+                         [("https://www90.zippyshare.com/v/T3CXRVue/file.html",
+                           "Batman 088 (2020).cbr")])
+def test_get_file_url(url, expected_name):
+    """Test get_file_url()."""
+    down_button = find_zippy_download_button(url)
+    _, name = get_file_url(url, down_button)
+    assert name == expected_name
+
+
+@pytest.mark.parametrize("old_name,new_name",
+                         [("Doomsday Clock 09 (of 12) (2019) (Webrip) (The Last Kryptonian-DCP).cbr",  # noqa: E501
+                           "Doomsday Clock 09 (of 12) (2019).cbr")])
+def test_remove_tag(old_name, new_name):
+    """Test remove tag."""
+    remove_tag_name = _remove_tag(old_name)
+    assert remove_tag_name == new_name
