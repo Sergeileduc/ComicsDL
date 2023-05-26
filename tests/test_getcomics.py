9e4d2ee76681e517a71111e8796e05593c3287f0
Sergeileduc <sergei.leduc@gmail.com>
2023-05-26 09:02:05 +0200

    add sleep so we don't get timeout

--- tests/test_getcomics.py
+++ tests/test_getcomics.py
@@ -2,6 +2,9 @@
 # -*-coding:utf-8 -*-
 """Unit tests for utils/getcomics."""
 
+import random
+from time import sleep
+
 import pytest
 
 from utils.getcomics import find_last_weekly, comics_list
@@ -21,6 +24,7 @@ def getcomics_url(request):
 
 def test_find_last_weekly(getcomics_url):
     """Test find_last_weekly."""
+    sleep(random.uniform(1.0, 2.0))
     myurl = find_last_weekly(getcomics_url)
     print(myurl)
     assert myurl is not None
@@ -38,12 +42,14 @@ def test_comicsList(getcomics_url):
     [("Batman", 0, 2, "https://getcomics.info/tag/batman/page/2/"),
      ("New X-Men", 1, 3, "https://getcomics.info/page/3/?s=new+x-men")])
 def test_searchurl(input_, mode, page, expected):
+    sleep(random.uniform(1.0, 2.0))
     myurl = searchurl(input_, mode, page)
     assert myurl == expected
 
 
 def test_get_results():
     """Test getresults."""
+    sleep(random.uniform(1.0, 2.0))
     searchurl = "https://getcomics.info/tag/batman/page/2/"
     searchlist = getresults(searchurl)
     print(*searchlist, sep='\n')
@@ -54,5 +60,6 @@ def test_get_results():
                            "Superman - Lost 02 (of 10) (2023).cbr")])
 def test_get_file_url(url: str, expected_name: str):
     """Test get_file_url()."""
+    sleep(random.uniform(1.0, 2.0))
     _, name, _ = getcomics_directlink(url)
     assert remove_tag(name) == expected_name
