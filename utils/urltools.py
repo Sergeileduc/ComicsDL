4780f32ff3a192432ee1d91ac47da058067e9bca
Sergeileduc <sergei.leduc@gmail.com>
2023-06-01 14:42:23 +0200

    urltools.py

--- utils/urltools.py
+++ utils/urltools.py
@@ -1,18 +1,16 @@
-#!/usr/bin/python3
-# -*-coding:utf-8 -*-
 """Some functions with urls."""
 
 import requests
 from requests.exceptions import HTTPError
 
-user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'  # noqa: E501
 headers = {'User-Agent': user_agent}
 
 
 def getfinalurl(url):
     """Follow redirections and get final url."""
     try:
-        response = requests.get(url)
+        response = requests.get(url, headers=headers)
         if response.history:
             print("Request was redirected")
             # for resp in response.history:
